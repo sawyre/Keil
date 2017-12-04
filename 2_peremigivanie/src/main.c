55e7c5178386a95e395c03528a08a2921e18fbd1
sawyre <sawure@mail.ru>
2017-12-04 13:41:47 +0300

    last changes

--- 2_peremigivanie/src/main.c
+++ 2_peremigivanie/src/main.c
@@ -2,15 +2,16 @@
 
 int main(void)
 {
-	RCC->APB2ENR|=1<<4;  //Подать питание на порт C
-	GPIOC->CRH|=0x22;		 //Настроить режим контактов 8 и 9 в порту C на режим General purpose output push pull
+	RCC->APB2ENR|=1<<4;  		//Подать питание на порт C
+	GPIOC->CRH&=0xFFFFFF00; //Настроить режим контактов 8 и 9 в порту C
+	GPIOC->CRH|=0x22;		 		//на режим General purpose output push pull
 	while(1){
-		GPIOC->ODR|=1<<8;	 //Подать высокий уровень
-		for(int32_t i=0;i<1000000000;i++)
+		GPIOC->ODR|=1<<8;	 //Подать высокий уровень на контакт 8
+		for(uint32_t i=0;i<1000000;i++)
 		{}
 		GPIOC->ODR&=~(1<<8);
-		GPIOC->ODR|=1<<9;		//Подать высокий уровень
-		for(int32_t i=0;i<1000000000;i++)
+		GPIOC->ODR|=1<<9;		//Подать высокий уровень на контакт 9
+		for(uint32_t i=0;i<1000000;i++)
 		{}
 		GPIOC->ODR&=~(1<<9); 
 	}
--- 3_po nagatiu treh knopok/src/main.c
+++ 3_po nagatiu treh knopok/src/main.c
@@ -3,13 +3,17 @@
 int main(void)
 {
 	RCC->APB2ENR|=1<<2; 		//Подать питание на порт A
-	GPIOA->CRL=0x22222220;	//Настроить режим контакта 8 в порт C на режим General purpose output push pull
-	GPIOA->CRH=0x2;	//Настроить режим контакта 8 в порт C на режим General purpose output push pull
+	GPIOA->CRL&=0x0000000F;	//Настроить режим 
+	GPIOA->CRH&=0xFFFFFFF0;	//контактов с 1 по 8 
+	GPIOA->CRL|=0x22222220;	//в порту А
+	GPIOA->CRH|=0x2;				//на режим General purpose output push pull
 	
 	RCC->APB2ENR|=1<<3;			//Подать питание на порт B
-	GPIOB->CRL=0x444;				//Настроить режим контакта 0 в порт C на режим Floating input (reset state)
+	GPIOB->CRL&=0xFFFFF000;	//Настроить режим контактов с 1 по 8 
+	GPIOB->CRL|=0x00000444;	//в порту B на режим Floating input
 	while(1){
-		GPIOA->ODR=1<<(GPIOB->IDR&7);		//Вывести на контакт закодированный кнопками (и только на него) высокий уровень
+		GPIOA->ODR&=0xFFFFFE01;														//Вывести на контакт закодированный кнопками
+		GPIOA->ODR|=0x000001FE&(1<<((GPIOB->IDR&7)+1));		//(и только на него) высокий уровень
 	}
 	return 0;
 }
