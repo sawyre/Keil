383cc4f697299b63f508ad08f53952f998bf2813
sawyre <sawure@mail.ru>
2018-03-06 13:51:00 +0300

    Mission 6

--- Mission 5/2_peremigivanie/src/main.c
+++ Mission 5/2_peremigivanie/src/main.c
@@ -1,19 +1,23 @@
 #include <stm32f10x.h>
 
-int main(void)
-{
+volatile uint32_t count=0;
+
+void SysTick_Handler(void) {
+	count++;
+}
+
+int main(void) {
 	RCC->APB2ENR|=1<<4;  		//Подать питание на порт C
-	GPIOC->CRH&=0xFFFFFF00; //Настроить режим контактов 8 и 9 в порту C
-	GPIOC->CRH|=0x22;		 		//на режим General purpose output push pull
-	while(1){
-		GPIOC->ODR|=1<<8;	 //Подать высокий уровень на контакт 8
-		for(uint32_t i=0;i<1000000;i++)
-		{}
-		GPIOC->ODR&=~(1<<8);
-		GPIOC->ODR|=1<<9;		//Подать высокий уровень на контакт 9
-		for(uint32_t i=0;i<1000000;i++)
-		{}
-		GPIOC->ODR&=~(1<<9); 
+	GPIOC->CRH&=0xFFFFFFF0; //Настроить режим контакта 8 в порту C
+	GPIOC->CRH|=0x2;		 		//на режим General purpose output push pull
+	__disable_irq();
+	SysTick_Config(16000000);			//Таймер для прерывания на 0.25 с
+	__enable_irq();
+	while(1) {
+		if(count > 8) {       //Каждые 2 секунды
+			GPIOC->ODR^= 1<<8;
+			count = 0;
+		}
 	}
 	return 0;
 }
